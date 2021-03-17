de45b8ee1c946d6635bb80eedd5cf018f0dac6ea
Mija Andrei <62014570+kizcko@users.noreply.github.com>
2021-03-17 23:53:36 +0200

    Add files via upload

--- lab3/main.py
+++ lab3/main.py
@@ -1,10 +1,13 @@
 import numpy as np
 from copy import copy, deepcopy
+
 epsilon = 10 ** float(-16)
+
 
 class matriceA:
     a = []
-    def citire_eficienta_a(self,filename):
+
+    def citire_eficienta_a(self, filename):
         with open(filename, 'r') as f:
             n = f.readline()
             a = [[] for i in range(int(n))]
@@ -25,14 +28,16 @@ class matriceA:
                 if not already:
                     a[i].append((val, j))
             self.a = a
-class matriceB:
 
+
+class matriceB:
     a = []
     b = []
     c = []
     p = None
     q = None
-    def citire_eficienta_b(self,filename):
+
+    def citire_eficienta_b(self, filename):
         with open(filename, 'r') as f:
             n = f.readline()
             p = f.readline()
@@ -43,72 +48,66 @@ class matriceB:
             b = []
             c = []
             for line in f:
-                if(line == '\n'):
+                if (line == '\n'):
                     switch = switch + 1
                     continue
-                if(switch == 0):
+                if (switch == 0):
                     a.append(float(line.rstrip('\n')))
-                if(switch == 1):
+                if (switch == 1):
                     b.append(float(line.rstrip('\n')))
-                if(switch == 2):
+                if (switch == 2):
                     c.append(float(line.rstrip('\n')))
         self.a = a
         self.b = b
         self.c = c
         self.p = p
         self.q = q
+
+
 class Matrix_operations:
     sum = []
-    def sum(self,matriceB, matriceA):
+    prod = []
+
+    def sum(self, matriceB, matriceA):
         acopy = [row[:] for row in matriceA.a]
 
         p = matriceB.p
         q = matriceB.q
         # for i in range(0,len(acopy)):
         #     for values in acopy[i]:
-        
+
         # line = linie
-        # j[0] = valoare 
+        # j[0] = valoare
         # j[1] = coloana
-        
-        for line,sub_list in enumerate(acopy):
-            for index,j in enumerate(sub_list):
-                if(j[1] == line):
-                    acopy[line][index] = (j[0] + matriceB.a[line],j[1])
-                if(j[1]-line == q):
-                    acopy[line][index] = (j[0] + matriceB.b[line],j[1])
-                if(line-j[1] == p):
-                    acopy[line][index] = (j[0] + matriceB.c[j[1]],j[1])
+
+        for line, sub_list in enumerate(acopy):
+            for index, j in enumerate(sub_list):
+                if (j[1] == line):
+                    acopy[line][index] = (j[0] + matriceB.a[line], j[1])
+                if (j[1] - line == q):
+                    acopy[line][index] = (j[0] + matriceB.b[line], j[1])
+                if (line - j[1] == p):
+                    acopy[line][index] = (j[0] + matriceB.c[j[1]], j[1])
         self.sum = acopy
-        
-    def multiply(self,matriceA,matriceB):
+
+    def multiply(self, matriceA, matriceB):
         sumcopy = [row[:] for row in matriceA.a]
         q = matriceB.q
         p = matriceB.p
-        # for line,sub_list in enumerate(matriceA.a):
-        #     for index,j in enumerate(sub_list):
-        #         if(line == 0):
-        #             sumcopy[line][index] = (j[0] * matriceB.a[j[1]] , j[1])
-        #             sumcopy[line][index] = sumcopy[line][2] * matriceB.c[0]
 
-        #         # if(line>0 and line < len(matriceA.a)):    
-        #         #     #
 
-        #         # if(line == len(matriceA.a)):
-        #         #     #
-        
-        for line,sub_list in enumerate(sumcopy):
-            for index,j in enumerate(sub_list):
-                if(j[1] == line):
-                    sumcopy[line][index] = (j[0] * matriceB.a[j[1]],j[1])
-                if(j[1]-line == q):
-                    sumcopy[line][index] = (j[0] * matriceB.b[j[1]-q+1],j[1])
-                if(line-j[1] == p):
-                    sumcopy[line][index] = (j[0] * matriceB.c[j[1]-p],j[1])
+        for line, sub_list in enumerate(sumcopy):
+            for index, j in enumerate(sub_list):
+                if (j[1] == line):
+                    sumcopy[line][index] = (j[0] * matriceB.a[j[1]], j[1])
+                if (j[1] - line == q):
+                    sumcopy[line][index] = (j[0] * matriceB.b[j[1] - q + 1], j[1])
+                if (line - j[1] == p):
+                    sumcopy[line][index] = (j[0] * matriceB.c[j[1] - p], j[1])
 
-        return sumcopy
+        self.sum = sumcopy
 
-    def compare_vectors(self,a,b):
+    def compare_vectors(self, a, b):
         for line_num, i in enumerate(a):
             for j in i:
                 not_found = True
@@ -121,54 +120,41 @@ class Matrix_operations:
                     return False
         return True
 
-    # def inmultesteMatrici(a, b):
-    # mat = []
-    # for i in range(len(a)):
-    #     linie = []
-    #     for j in range(len(b)):
-    #         sum = 0
-    #         for l in range(len(a[i])):
-    #             bb = 0
-    #             for x in range(len(b[a[i][l][1]])):
-    #                 if b[a[i][l][1]][x][1] == j % (len(b)):
-    #                     bb = b[a[i][l][1]][x][0]
-    #                     break
-
-    #             sum = sum + bb * a[i][l][0]
-    #         if sum != 0:
-    #             linie.append([sum, j])
-    #     mat.append(linie)
-
-    # return mat       
 
 
 a = matriceA
-a.citire_eficienta_a(a,"res/a.txt")
+a.citire_eficienta_a(a, "res/a.txt")
 
 b = matriceB
-b.citire_eficienta_b(b,"res/b.txt")
+b.citire_eficienta_b(b, "res/b.txt")
+
+
+# # ----------------------------------------------------------------
+# Adunare
+print("\nAdunare:")
+
 
 operations = Matrix_operations()
-operations.sum(b,a)
+operations.sum(b, a)
 
 aplusb = matriceA
-aplusb.citire_eficienta_a(aplusb,"res/aplusb.txt")
-#print(operations.sum[2020])
+aplusb.citire_eficienta_a(aplusb, "res/aplusb.txt")
+print(operations.compare_vectors(aplusb.a, a.a))
+print("\n")
+
+# # ----------------------------------------------------------------
+# Inmultire
+
+print("Inmultire: ")
+a1 = matriceA
+a1.citire_eficienta_a(a1, "res/a.txt")
+
 aorib = matriceA
-aorib.citire_eficienta_a(aorib,"res/aorib.txt")
+aorib.citire_eficienta_a(aorib, "res/aorib.txt")
 
-print(operations.compare_vectors(aplusb.a,a.a))
-
-#----------------------------------------------------------------
-
-anew = matriceA
-anew.citire_eficienta_a(anew,"res/a.txt")
-# print(anew.a[0])
-# print(b.a[0])
-# print(b.c[0])
-# print(b.b[0])
-# print(anew.a[1])
-print(aorib.a[0])
-print(operations.multiply(anew,b)[0])
-
-
+operations = Matrix_operations()
+operations.multiply(a1,b)
+# print(a1.a[1])
+# print(aorib.a[1])
+print(operations.compare_vectors(aorib.a, a1.a))
+print("\n")
