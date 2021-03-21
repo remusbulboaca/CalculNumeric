a45e8667762043f2166a9b61137ba605d2bfe5c0
remusbulboaca <47847935+remusbulboaca@users.noreply.github.com>
2021-03-21 09:43:47 +0200

    .

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,4 +1,5 @@
 import math
+from math import pi,tan
 import random
 import time
 # Ex 1
@@ -12,6 +13,29 @@ def ex1():
         m = m - 1
     mach = 10**(m+1)
     return mach
+
+def my_tan(x, eps):
+  sign = 1
+  if x < 0:
+    x *= -1
+    sign = -1
+  mic = 10**(-12)
+  c = f = mic
+  d = 0
+  j = 1
+  a = x 
+  while True:
+    b = 2*(j-1) + 1
+    d = max(b + a*d, mic)
+    c = max(b + a/c, mic)
+    a = -x**2
+    d = 1 / d
+    delta = c*d
+    f = delta*f
+    j = j + 1   
+    if abs(delta - 1) <= eps: 
+      break
+  return sign*f
 
 #Ex 2.a
 def ex2a():
@@ -33,4 +57,13 @@ def ex2b():
     else:
         return "[ X C ] Neasociativa!"
 
+t1 = time.time()
+for i in range(0,10000):
+    x = random.uniform(-pi/2,pi/2)
+    sum = abs(tan(x) - my_tan(x, ex1()))
+t2 = time.time()
 
+time = t2 - t1
+err = sum / 10000
+print(time)
+print(err)
--- lab1/tempCodeRunnerFile.py
+++ lab1/tempCodeRunnerFile.py
@@ -1,7 +1,30 @@
-from tangenta import *
-# import time
-# from generator import *
-# from lab1 import *
-# from tkinter import *
-# from tkinter import messagebox
+x 2.a
+def ex2a():
+    a = 1.0
+    b = ex1()/10
+    c = ex1()/10
+    if (a + b) + c == a + (b + c):
+        return "[ + C ] Asociativa"
+    else:
+        return "[ + C ] Neasociativa"
 
+#Ex 2.b
+def ex2b():
+    a = 0.2
+    b = ex1()/10
+    c = ex1()/10
+    if (a * b) * c == a * (b * c):
+        return "[ X C ] Asociativa!"
+    else:
+        return "[ X C ] Neasociativa!"
+
+t1 = time.time()
+for i in range(0,10000):
+    x = random.uniform(-pi/2,pi/2)
+    sum = abs(tan(x) - my_tan(x, 1000))
+t2 = time.time()
+
+time = t2 - t1
+err = sum / 10000
+print(time)
+print(err)
